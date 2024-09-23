d0a9bdc478bc945351c53f6d692ce1bf736b16fe
Alexander Netz <alex.netz1@web.de>
2024-09-23 14:57:50 +0200

    extracted subscription

--- internal/uniquejob/executor.go
+++ internal/uniquejob/executor.go
@@ -8,64 +8,6 @@ import (
 
 type ExecutorFunc[R any] func(context.Context) (R, error)
 
-type Subscription[R any] struct {
-	result       chan R
-	errorChannel chan error
-	m            sync.Mutex
-}
-
-func NewSubscription[R any]() *Subscription[R] {
-	return &Subscription[R]{
-		result:       make(chan R),
-		errorChannel: make(chan error),
-	}
-}
-
-func (s *Subscription[R]) Subscribe(ctx context.Context) (R, error) {
-
-	var res R
-
-	defer s.close()
-
-	select {
-	case res = <-s.result:
-		return res, nil
-	case err := <-s.errorChannel:
-		return res, err
-	case <-ctx.Done():
-		return res, errors.New("context closed")
-	}
-}
-
-func (s *Subscription[R]) send(result R, err error) {
-	s.m.Lock()
-	defer s.m.Unlock()
-
-	select {
-	case <-s.result:
-		return
-	case <-s.errorChannel:
-		return
-	default:
-	}
-
-	if err != nil {
-		s.errorChannel <- err
-		return
-	}
-
-	s.result <- result
-
-}
-
-func (s *Subscription[R]) close() {
-	s.m.Lock()
-	defer s.m.Unlock()
-
-	close(s.result)
-	close(s.errorChannel)
-}
-
 type JobExecutor[R any, K comparable] struct {
 	workingSet map[K]*Job[R, K]
 	m          sync.RWMutex
